040f5ac15af90a83434a9a730d2335db5f2bc3c2
i544c <leo@i544c.me>
2018-03-24 15:14:14 +0900

    antdを追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
 import './App.css';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">
+              <span className="nav-text">Top</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff", padding: 0 }}>Fuck the h4k0bu5</Header>
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div style={{ padding: 24, background: "#fff", minHeight: "calc(100vh - 157px)" }}>
+              <p>Hello</p>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            &copy;2018 Isaac
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
