b687a7cf1a8b45840105a845ba3711dcabd9befa
i544c <leo@i544c.me>
2018-04-04 20:30:14 +0900

    Aboutを追加

---
+++ frontend/src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      函館バス，あぁ函館バス．お前は何故自ら利用者を減らそうとする．
+      Not定時運行は当たり前，接近情報から姿を晦ます．
+      2018年3月にリニューアルしたバスロケーションシステムは，やっとスマホ対応したものの相変わらず見づらい．
+    </p>
+
+    <p>
+      函館唯一のバス会社なのだから，そんなのでは困る．
+      <a href="https://mainichi.jp/articles/20170130/k00/00e/040/167000c" rel="noopener noreferrer" target="_blank">そりゃ幸福度最下位の都市にもなる．</a>
+    </p>
+
+    <p>
+      このサイトは，少しでも函館バスの接近情報を見やすくしたものである．
+      意見，要望ならTwitterで <a href="https://twitter.com/intent/tweet?text=%23fuck_the_hakobus" rel="noopener noreferrer" target="_blank">#fuck_the_hakobus</a> を付けて呟いてほしい．
+    </p>
+  </div>
+);
